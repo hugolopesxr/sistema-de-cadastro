2481db2f9400075f4f3db13edeb176bd4d4f1921
hugolopesxr <hugo_xr@hotmail.com>
2023-02-23 14:10:22 -0300

    adicionando 2 console log

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Backend iniciado com sucesso!")
+//mais um console log de teste
+console.log("Hugo fez esse commit sem você ver na aula!")
+console.log("Enviando para o GitHub")
